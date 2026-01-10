a6389d78e62d7aeae352075aeaaf700a1d4c30bd
Suraj Kumar <w4rsuraj99@gmail.com>
2026-01-10 11:10:23 +0530

    fix: load environment variables before app imports

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
-import registerRouter from './routes/auth.route.js';
+import {loginRouter, registerRouter} from './routes/auth.route.js';
+import './config/env.js'
 
 dotenv.config();
 
@@ -15,7 +16,11 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 
+//register
 app.use('/api',registerRouter)
+
+//login
+app.use('/api',loginRouter)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
