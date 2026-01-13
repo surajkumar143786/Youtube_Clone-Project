b1334a0b2d2522174b7ec26d70fc36bce9e88fd1
Suraj Kumar <w4rsuraj99@gmail.com>
2026-01-13 16:36:42 +0530

    add header layout using outlet

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 function App(){
   return(
-    <Outlet/>
+    <div>
+      <Outlet />
+      <Header />
+    </div>
   )
 }
 export default App;
--- Frontend/src/components/Body.jsx
+++ Frontend/src/components/Body.jsx
@@ -1,8 +1,24 @@
 function Body(){
     return(
-        <div>
-            <h1>Body</h1>
+        <header className="flex items-center justify-between px-4 py-2 border-b">
+            {/* youtube logo */}
+            <div className="text-xl font-bold">
+                youtube
+            </div>
+        
+        {/* search box */}
+        <div className="w-1/2">
+            <input type="text" placeholder="Search"
+            className="w-full px-3 py-1 border rounded-full outline-none"/>
         </div>
+
+        {/* signIn button */}
+            <button className="px-4 py-1 border rounded-full text-blue-600 font-medium">
+            SignIn
+            </button>
+
+
+        </header>
     )
 }
 export default Body;
