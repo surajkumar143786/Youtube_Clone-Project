f637a64a31a95ed15bc6a455a68c00bc3703455c
Suraj Kumar <w4rsuraj99@gmail.com>
2026-01-14 16:20:51 +0530

    fix Header.jsx and fix some bugs

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { useState } from "react";
 
 function App(){
+  const [searchText,setSearchText] = useState("")
   return(
     <div>
-      <Header />
+      <Header  onSearch= {setSearchText}/>
       {/* Main content area */}
       <div className="flex">
         <Sidebar />
         <div className="flex-1 p-4">
-          <Outlet />
+          <Outlet context={{searchText}}/>
         </div>
       </div>
     </div>
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,8 +1,37 @@
-function Header(){
-    return(
-        <div>
-           
-        </div>
-    )
+import { useState } from "react";
+
+function Header() {
+    const [searchText,setSearchText] = useState("")
+    return (
+        <header className="flex items-center justify-between px-4 py-2 border-b">
+
+            {/* Logo */}
+            <div className="text-xl font-bold text-red-500">
+                YouTube
+            </div>
+
+            {/* Search Box */}
+            <div className="w-1/2">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    value={searchText}
+                    onChange={(e)=>{
+                        setSearchText(e.target.value);
+                        onChange(e.target.value)     //send text to parent
+                    }}
+                    className="w-full px-3 py-1 border rounded-full outline-none"
+                    
+                />
+            </div>
+
+            {/* Sign In Button */}
+            <button className="px-4 py-1 border rounded-full text-blue-600 font-medium">
+                Sign In
+            </button>
+
+        </header>
+    );
 }
+
 export default Header;
